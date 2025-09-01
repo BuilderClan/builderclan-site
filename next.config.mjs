72b546b3ec637f1d9345146d0c03f4dfc8c847b7
Alvin Dennis <alvindennis80@gmail.com>
2025-09-01 23:20:43 +0530

    feat: remove eduquest paths

--- next.config.mjs
+++ next.config.mjs
@@ -8,17 +8,6 @@ const nextConfig = {
         permanent: false,
       },
       {
-        source: "/eduquest/register",
-        destination: "https://app.formbricks.com/s/x7km1hi7s9gn39eyokv91qxk", // replace with actual form URL
-        permanent: false,
-      },
-      {
-        source: "/eduquest/guide",
-        destination: "https://www.notion.so/builderclan/EduQuest-The-24-Hour-Game-Based-Learning-Hackathon-2427f33836f080029c2fc61e8cc091b1", // replace with actual Notion page
-        permanent: false,
-      },
-
-      {
         source: "/:path*",
         has: [
           {
